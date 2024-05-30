e2a1f0675165edc8bac3c6a09b37780f56aad1e7
Daniil-Ka <dkrapivin.04@gmail.com>
2024-05-31 00:18:51 +0500

    большие изменения в фронте

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+import users
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('algorithm/', include("algorithm.urls")),
     path('users/', include("users.urls")),
+
+    path('for_musicians/', users.views.ForMusiciansView.as_view()),
+    path('about/', users.views.AboutView.as_view()),
+    path('docs/', users.views.DocsView.as_view()),
 
     path('', RedirectView.as_view(url='/users/', permanent=True))
 ]
