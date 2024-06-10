bdd3a531670aeb6ecbdcbde455c1cab54dc7eaf1
Daniil-Ka <dkrapivin.04@gmail.com>
2024-06-10 11:35:58 +0500

    фикс куки

--- static_root/js/history.js
+++ static_root/js/history.js
@@ -1,19 +1,20 @@
 let songHistoryCounter = 0;
 
-// Функция для сохранения истории песен в куки
+// Функция для сохранения истории песен в локальное хранилище
 function saveSongsHistory(history) {
-    Cookies.set(`songsHistory${songHistoryCounter}`, JSON.stringify(history), { expires: 7 }); // Сохранение на 7 дней
+    localStorage.setItem(`songsHistory${songHistoryCounter}`, JSON.stringify(history));
     songHistoryCounter++; // Увеличиваем счетчик для следующей истории песен
+    localStorage.setItem('songHistoryCounter', songHistoryCounter); // Сохраняем текущее значение счетчика
 }
 
-// Функция для загрузки полной истории из куки
+// Функция для загрузки полной истории из локального хранилища
 function loadFullSongsHistory() {
     let history = [];
     let i = 0;
     while (true) {
-        const historyCookie = Cookies.get(`songsHistory${i}`);
-        if (!historyCookie) break;
-        history.push(JSON.parse(historyCookie));
+        const historyItem = localStorage.getItem(`songsHistory${i}`);
+        if (!historyItem) break;
+        history.push(JSON.parse(historyItem));
         i++;
     }
     return history;
@@ -26,3 +27,4 @@ function AddSongToHistory(jsonData) {
 // Загрузка полной истории при загрузке страницы
 let fullSongsHistory = loadFullSongsHistory();
 songHistoryCounter = fullSongsHistory.length;
+console.log(fullSongsHistory);
